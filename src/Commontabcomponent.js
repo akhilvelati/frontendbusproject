5af7ff7f7b6076b1832c807e74ef201bb6d5f582
pratyushjs <pratyush813@gmail.com>
2022-05-26 17:50:55 +0530

    tab changes

--- src/Commontabcomponent.js
+++ src/Commontabcomponent.js
@@ -5,16 +5,73 @@ import Login from "./Login";
 import { Route, Routes } from "react-router-dom";
 import User from "./User";
 import Admin from "./Admin";
-import { FormFeedback, InputGroup } from "reactstrap";
-function CommonTab({ headingName }) {
+import {
+  Button,
+  Col,
+  Form,
+  FormGroup,
+  Input,
+  InputGroup,
+  Label,
+  Row,
+} from "reactstrap";
+function CommonTab({ tabType }) {
+  const determineTabType = () => {
+    console.log(tabType);
+    if (tabType == 1) return "admin";
+    if (tabType == 2) return "studet";
+    if (tabType == 3) return "driver";
+  };
   return (
     <>
       <div className="CommonTab-add">
-        <h2 className="CommonTab-add-h2">Add {headingName}</h2>
+        <h2 className="CommonTab-add-h2">Add {determineTabType()}</h2>
+
+        <Form>
+          <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="exampleEmail">Email</Label>
+                <Input
+                  id="exampleEmail"
+                  name="name"
+                  placeholder="with a placeholder"
+                  type="text"
+                />
+              </FormGroup>
+            </Col>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="examplePassword">Password</Label>
+                <Input
+                  id="examplePassword"
+                  name="password"
+                  placeholder="password placeholder"
+                  type="text"
+                />
+              </FormGroup>
+            </Col>
+          </Row>
+
+          <Button>Addd</Button>
+        </Form>
       </div>
-      <div className="CommonTab-search"></div>
-      <div className="btn">
-        <button></button>
+      <div className="CommonTab-search">
+<br/>
+<br/>
+      <Row>
+            <Col md={6}>
+              <FormGroup>
+                <Label for="exampleEmail">Search</Label>
+                <Input
+                  id="exampleEmail"
+                  name="name"
+                  placeholder="with a placeholder"
+                  type="text"
+                />
+              </FormGroup>
+            </Col>
+            </Row>
       </div>
     </>
   );
