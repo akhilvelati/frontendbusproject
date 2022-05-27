7ee8e76424ac126c73e9a0f51c2b1457a991c7ae
akhilvelati <20r11a05c3@gcet.edu.in>
2022-05-26 17:33:16 -0700

    code changes

--- src/Commontabcomponent.js
+++ src/Commontabcomponent.js
@@ -1,41 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import LoginForm from './LoginForm';
-import Login from './Login';
-import { Route, Routes } from 'react-router-dom'
-  ;
-import User from './User';
-import Admin from './Admin';
-import { InputGroup, FormControl, } from 'reactstrap';
-function CommonTab({ headingName}) {
+import logo from "./logo.svg";
+import "./App.css";
+import LoginForm from "./LoginForm";
+import Login from "./Login";
+import { Route, Routes } from "react-router-dom";
+import User from "./User";
+import Admin from "./Admin";
+import { FormFeedback, InputGroup } from "reactstrap";
+function CommonTab({ headingName }) {
   return (
-      <>
-          <div className='CommonTab-add'>
-              
-              <h2 className='CommonTab-add-h2'>Add {headingName}</h2>
-              <InputGroup className="mb-3">
-    <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-    <FormControl
-      placeholder="Username"
-      aria-label="Username"
-      aria-describedby="basic-addon1"
-    />
-  </InputGroup>
-
-  <InputGroup className="mb-3">
-    <FormControl
-      placeholder="Recipient's username"
-      aria-label="Recipient's username"
-      aria-describedby="basic-addon2"
-    />
-    <InputGroup.Text id="basic-addon2">@example.com</InputGroup.Text>
-  </InputGroup>
-
-          </div>
-          <div className='CommonTab-search'></div>
-          <div className='btn'><button>
-          </button></div>
-      </>
+    <>
+      <div className="CommonTab-add">
+        <h2 className="CommonTab-add-h2">Add {headingName}</h2>
+      </div>
+      <div className="CommonTab-search"></div>
+      <div className="btn">
+        <button></button>
+      </div>
+    </>
   );
 }
 
